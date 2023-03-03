ca4a34470c5b465e3d18f0b70b24da1f3b72f04f
Ruan Silva <ruansilva6721@gmail.com>
2023-03-03 01:12:17 -0300

    adicionando Indicação e fábricaAluno

--- src/Aluno.php
+++ src/Aluno.php
@@ -5,5 +5,16 @@ class Aluno{
     private $cpf;
     private $nome;
     private $email;
+    private array $telefones;
+    public function __construct(Cpf $cpf, string $nome, Email $email)
+    {
+        $this->cpf = $cpf;
+        $this->nome = $nome;
+        $this->email = $email;
+    }
+    public function adicionarTelefone(string $ddd, string $numero)
+    {
+        $this->telefones[] = new Telefone($ddd, $numero);
+    }
 
 }
---
+++ src/FabricaAluno.php
@@ -0,0 +1,3 @@
+<?php 
+namespace Ruan\Arquitetura;
+
